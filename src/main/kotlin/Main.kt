a6836e5aa624c0d89aebcd52d970d73f87c10e0c
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-02-12 15:18:35 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var name = "Joan Muthoni"
+    println(name)
+    var age = "24"
+    println(age)
+    var phonenumber = "07212579345"
+    println(phonenumber)
+    var weight = "65"
+    println(weight)
+    val kenyan = true
+    println(kenyan)
+    val nonkenyan = false
+    println(nonkenyan)
+}
